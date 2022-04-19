2db31140ae14e6bf7aad112e6555492c06791150
Sergei Blinov <blinovsv@gmail.com>
2022-04-19 06:30:49 +0300

    buildkit: reworked publisher example

--- buildkit/examples/publisher/build.sh
+++ buildkit/examples/publisher/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-TARGET_IMAGE=teamgosh/sample-target-image
 NETWORKS="${NETWORKS:-https://gra01.net.everos.dev,https://rbx01.net.everos.dev,https://eri01.net.everos.dev}"
+TARGET_IMAGE=teamgosh/sample-target-image
+REPO_DIR=example1
+GOSH_ADDRESS=gosh::net.ton.dev://0:a6af961f2973bb00fe1d3d6cfee2f2f9c89897719c2887b31ef5ac4fd060638f/gosh/example
 
-if [[ -z "$WALLET" ]] || [[ -z "$WALLET_PUBLIC" ]] || [[ -z "$WALLET_SECRET" ]] ; then
+if [[ -z "$WALLET" ]] || [[ -z "$WALLET_PUBLIC" ]] || [[ -z "$WALLET_SECRET" ]]; then
     echo "Make sure \$WALLET \$WALLET_PUBLIC \$WALLET_SECRET are set"
     echo "export WALLET=..."
     echo "export WALLET_SECRET=..."
@@ -12,47 +14,50 @@ if [[ -z "$WALLET" ]] || [[ -z "$WALLET_PUBLIC" ]] || [[ -z "$WALLET_SECRET" ]] 
     exit
 fi
 
-echo ===================================
-echo Run buildkitd containered
-echo ===================================
+step() {
+    echo
+    echo
+    echo "$@"
+    echo
+}
 
-docker rm -f buildkitd || true
-docker run -d --name buildkitd --network host --privileged moby/buildkit:latest
+step 1. Clone GOSH repo
 
-echo ===================================
-echo Build $TARGET_IMAGE
-echo ===================================
+docker run --rm -ti \
+    -v "$(pwd)":/root \
+    teamgosh/git-remote-gosh \
+    clone \
+    "$GOSH_ADDRESS" \
+    "$REPO_DIR"
 
-buildctl --addr=docker-container://buildkitd build \
-    --frontend gateway.v0 \
-    --local dockerfile=. \
-    --local context=. \
-    --opt source=teamgosh/goshfile \
-    --opt filename=goshfile.yaml \
-    --opt wallet_public="$WALLET_PUBLIC" \
-    --output type=image,name="$TARGET_IMAGE",push=true
+step 2. Get current commit hash
 
-echo ===================================
-echo Pull $TARGET_IMAGE after buildctl
-echo ===================================
+cd "$REPO_DIR"
+GOSH_COMMIT_HASH=$(git rev-parse HEAD)
+cd ..
+echo Current commit hash: "$GOSH_COMMIT_HASH"
 
-docker pull $TARGET_IMAGE
+step 3. Build the image from GOSH repo
 
-TARGET_IMAGE_SHA=$(docker inspect --format='{{index (split (index .RepoDigests 0) "@") 1}}' $TARGET_IMAGE)
-echo TARGET_IMAGE_SHA=\'"$TARGET_IMAGE_SHA"\'
+docker buildx build \
+    -f example1/goshfile.yaml \
+    -t "$TARGET_IMAGE" \
+    --label WALLET_PUBLIC="$WALLET_PUBLIC" \
+    --label GOSH_ADDRESS="$GOSH_ADDRESS" \
+    --label GOSH_COMMIT_HASH="$GOSH_COMMIT_HASH" \
+    --push \
+    "$REPO_DIR"
 
-if [[ -z "$TARGET_IMAGE_SHA" ]] ; then
-    echo Target image hash not found
-    exit
-fi
+step 4. Get image GOSH_HASH
 
-echo ===================================
-echo Sign image
-echo ===================================
+GOSH_IMAGE_SHA=$(../../../docker-extension/vm/commands/gosh-image-sha.sh "$TARGET_IMAGE")
+echo GOSH_IMAGE_SHA "$GOSH_IMAGE_SHA"
+
+step 5. Sign the image
 
 docker run --rm teamgosh/sign-cli sign \
     -n "$NETWORKS" \
     -g "$WALLET" \
     -s "$WALLET_SECRET" \
     "$WALLET_SECRET" \
-    "$TARGET_IMAGE_SHA"
+    "$GOSH_IMAGE_SHA"
