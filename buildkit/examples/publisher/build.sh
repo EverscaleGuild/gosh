6bc41b742aee890580ff547a568dfae73f1c87a6
Sergei Blinov <blinovsv@gmail.com>
2022-04-19 15:07:28 +0300

    update publisher example

--- buildkit/examples/publisher/build.sh
+++ buildkit/examples/publisher/build.sh
@@ -3,8 +3,10 @@ set -e
 
 NETWORKS="${NETWORKS:-https://gra01.net.everos.dev,https://rbx01.net.everos.dev,https://eri01.net.everos.dev}"
 TARGET_IMAGE=teamgosh/sample-target-image
-REPO_DIR=example1
-GOSH_ADDRESS=gosh::net.ton.dev://0:a6af961f2973bb00fe1d3d6cfee2f2f9c89897719c2887b31ef5ac4fd060638f/gosh/example
+REPO_NAME=demo-100
+GOSH_NETWORK=net.ton.dev
+GOSH_ROOT_CONTRACT=0:2f4ade4a98f916f47b1b2ff7abe1ee8a096d8443754b01b092d5043aa8ba1c8e
+GOSH_ADDRESS=gosh::"$GOSH_NETWORK"://"$GOSH_ROOT_CONTRACT"/"$REPO_NAME"
 
 if [[ -z "$WALLET" ]] || [[ -z "$WALLET_PUBLIC" ]] || [[ -z "$WALLET_SECRET" ]]; then
     echo "Make sure \$WALLET \$WALLET_PUBLIC \$WALLET_SECRET are set"
@@ -28,25 +30,30 @@ docker run --rm -ti \
     teamgosh/git-remote-gosh \
     clone \
     "$GOSH_ADDRESS" \
-    "$REPO_DIR"
+    "$REPO_NAME"
 
 step 2. Get current commit hash
 
-cd "$REPO_DIR"
+cd "$REPO_NAME"
 GOSH_COMMIT_HASH=$(git rev-parse HEAD)
 cd ..
+
 echo Current commit hash: "$GOSH_COMMIT_HASH"
 
 step 3. Build the image from GOSH repo
 
+cd "$REPO_NAME"
+
 docker buildx build \
-    -f example1/goshfile.yaml \
+    -f goshfile.yaml \
     -t "$TARGET_IMAGE" \
     --label WALLET_PUBLIC="$WALLET_PUBLIC" \
     --label GOSH_ADDRESS="$GOSH_ADDRESS" \
     --label GOSH_COMMIT_HASH="$GOSH_COMMIT_HASH" \
     --push \
-    "$REPO_DIR"
+    .
+
+cd ..
 
 step 4. Get image GOSH_HASH
 
