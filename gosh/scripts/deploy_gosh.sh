12cb9cc69ee0bbf3302be53cb523a104c988fdbb
Gorelyshev Sergey <49919055+SeHor05@users.noreply.github.com>
2022-04-24 09:40:00 +0200

    Update deploy_gosh.sh

--- gosh/scripts/deploy_gosh.sh
+++ gosh/scripts/deploy_gosh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#	This file is part of Ever OS.
-#	
-#	Ever OS is free software: you can redistribute it and/or modify 
-#	it under the terms of the Apache License 2.0 (http://www.apache.org/licenses/)
-#	
-#	Copyright 2019-2022 (c) EverX
+#       This file is part of Ever OS.
+#
+#       Ever OS is free software: you can redistribute it and/or modify
+#       it under the terms of the Apache License 2.0 (http://www.apache.org/licenses/)
+#
+#       Copyright 2019-2022 (c) EverX
 
 source utils.sh
 
@@ -78,6 +78,31 @@ echo "load \`tag\`-contract"
 TAG_CODE=$($TVM_LINKER decode --tvc ../tag.tvc | sed -n '/code:/ s/ code: // p')
 TAG_DATA=$($TVM_LINKER decode --tvc ../tag.tvc | sed -n '/data:/ s/ data: // p')
 $TONOS_CLI -u $NETWORK call $GOSH_ADDR setTag "{\"code\":\"$TAG_CODE\",\"data\":\"$TAG_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
+
+echo "load \`TokenWallet\`-contract"
+TokenWallet_CODE=$($TVM_LINKER decode --tvc ../../smv/External/tip3/TokenWallet.tvc | sed -n '/code:/ s/ code: // p')
+TokenWallet_DATA=$($TVM_LINKER decode --tvc ../../smv/External/tip3/TokenWallet.tvc | sed -n '/data:/ s/ data: // p')
+$TONOS_CLI -u $NETWORK call $GOSH_ADDR setTokenWallet "{\"code\":\"$TokenWallet_CODE\",\"value1\":\"$TokenWallet_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
+
+echo "load \`SMVTokenLocker\`-contract"
+SMVTokenLocker_CODE=$($TVM_LINKER decode --tvc ../../smv/SMVTokenLocker.tvc | sed -n '/code:/ s/ code: // p')
+SMVTokenLocker_DATA=$($TVM_LINKER decode --tvc ../../smv/SMVTokenLocker.tvc | sed -n '/data:/ s/ data: // p')
+$TONOS_CLI -u $NETWORK call $GOSH_ADDR setTokenLocker "{\"code\":\"$SMVTokenLocker_CODE\",\"value1\":\"$SMVTokenLocker_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
+
+echo "load \`LockerPlatform\`-contract"
+LockerPlatform_CODE=$($TVM_LINKER decode --tvc ../../smv/LockerPlatform.tvc | sed -n '/code:/ s/ code: // p')
+LockerPlatform_DATA=$($TVM_LINKER decode --tvc ../../smv/LockerPlatform.tvc | sed -n '/data:/ s/ data: // p')
+$TONOS_CLI -u $NETWORK call $GOSH_ADDR setSMVPlatform "{\"code\":\"$LockerPlatform_CODE\",\"value1\":\"$LockerPlatform_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
+
+echo "load \`SMVClient\`-contract"
+SMVClient_CODE=$($TVM_LINKER decode --tvc ../../smv/SMVClient.tvc | sed -n '/code:/ s/ code: // p')
+SMVClient_DATA=$($TVM_LINKER decode --tvc ../../smv/SMVClient.tvc | sed -n '/data:/ s/ data: // p')
+$TONOS_CLI -u $NETWORK call $GOSH_ADDR setSMVClient "{\"code\":\"$SMVClient_CODE\",\"value1\":\"$SMVClient_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
+
+echo "load \`SMVProposal\`-contract"
+SMVProposal_CODE=$($TVM_LINKER decode --tvc ../../smv/SMVProposal.tvc | sed -n '/code:/ s/ code: // p')
+SMVProposal_DATA=$($TVM_LINKER decode --tvc ../../smv/SMVProposal.tvc | sed -n '/data:/ s/ data: // p')
+$TONOS_CLI -u $NETWORK call $GOSH_ADDR setSMVProposal "{\"code\":\"$SMVProposal_CODE\",\"value1\":\"$SMVProposal_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
 
 echo ===================== GOSH =====================
 echo "   addr:" $GOSH_ADDR
