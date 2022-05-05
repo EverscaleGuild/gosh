592bfa8e42147547f7eb4efbe027c9488b64690e
Sergei Blinov <blinovsv@gmail.com>
2022-05-05 21:59:49 +0300

    ipfs: use dns peers; fix pins logic via ipfs-utils

--- git-remote-gosh/ipfs.js
+++ git-remote-gosh/ipfs.js
@@ -1,9 +1,11 @@
+//@ts-check
 const all = require('it-all')
 const ipfs = require('ipfs-core')
 const Protector = require('libp2p/src/pnet')
 const { setTimeout } = require('timers/promises')
 const { BetterLock } = require('better-lock');
 const { verbose } = require('./utils')
+const HTTP = require('ipfs-utils/src/http')
 
 
 const SWARM_KEY = `/key/swarm/psk/1.0.0/
@@ -11,16 +13,21 @@ const SWARM_KEY = `/key/swarm/psk/1.0.0/
 0aa5e0806b7a6abf19f6e63689d3e6434df132571539b4ca7e5a1cbf4e3f8583`
 
 const BOOTSTRAP_SERVERS = [
-    "/ip4/51.91.62.120/tcp/4001/p2p/12D3KooWBjVe3SJcuHTdvZsSxPfAWfTSPHhqRP1GR7iqFiN9QcqY",
-    "/ip4/51.210.114.148/tcp/4001/p2p/12D3KooWHoVMvnDm8hxGGiZdFH1WEkuaF3YxyF1A5JyifdYKi4Aq",
+    "/dns4/n01.network.gosh.sh/tcp/4001/ipfs/12D3KooWKfrNXN3msmrd5SYP6yFmWypGf2aicxPzJ1v2RXiRVCDk",
+    "/dns4/n02.network.gosh.sh/tcp/4001/ipfs/12D3KooWDvM2UE9ZsPQ9nC7QoVsrd9uVLuzNV2gBXa9zLpt67AHb",
+    "/dns4/n03.network.gosh.sh/tcp/4001/ipfs/12D3KooWBvUFBdT1uYn8ftHjkKb193LRfndNXZejEyngQ2cHGfyF",
+    "/dns4/n04.network.gosh.sh/tcp/4001/ipfs/12D3KooWA9CWkyYfv5kBcabYnKB5HH81WzcQ4uBjop9i6iPDbuYt",
+    "/dns4/n05.network.gosh.sh/tcp/4001/ipfs/12D3KooWRXRmXrWmkDd3nzo3c25f9BifNf2qdJtBto6vn77jyveg",
+    "/dns4/n06.network.gosh.sh/tcp/4001/ipfs/12D3KooWAYrCWysudygmwxKTjKxdBFCQdkP7d44Uh7xP9JgCteKb",
+    "/dns4/n07.network.gosh.sh/tcp/4001/ipfs/12D3KooWRwHJUJ7ypnEJ8VYMCrBQXynsmwypD7yECfCguV5QVLV3",
+    "/dns4/n08.network.gosh.sh/tcp/4001/ipfs/12D3KooWDdQq6ZkBspUFuCAFr2MAoTjhDfGyMtcqq9vXrgdHFGPC",
+    "/dns4/n09.network.gosh.sh/tcp/4001/ipfs/12D3KooWQVLdvT351X3aPqLYH8C3FvHiE5AGsPRPncMJFVd26xiD",
 ]
 
-const PIN_REMOTE_ENDPOINT = "http://pin.ipfs.gosh.sh:9097/pins"
-
+const PIN_REMOTE_ENDPOINT = "http://pin.ipfs.gosh.sh:9097"
+const PIN_BEARER_KEY = ""
 
 const ipfs_lock = new BetterLock()
-
-
 let ipfs_node = undefined
 
 // ~ UPDATE_INTERVAL * MAX_NETWORK_ERRORS seconds of doing nothing
@@ -28,67 +35,72 @@ const UPDATE_INTERVAL = 1000 // ms
 const MAX_NETWORK_ERRORS = 30
 let networkErrors = 0
 
+/** @type {HTTP} */
+const PIN_API = new HTTP({
+    base: PIN_REMOTE_ENDPOINT,
+    headers: {
+        Authorization: `Bearer ${PIN_BEARER_KEY}`,
+        'Content-Type': 'application/json'
+    }
+})
+
 /**
  * Pin remote CID
  * !! Very unreliable method so we do many retries
  * @example
  *      curl -X POST -H "Content-Type: application/json" \
  *        -d '{"cid": "QmUtV6dzFDS6JNkj2e2dp5vxQPogdF58zQYwRL5v1LGEQe"}' \
- *        http://pin.ipfs.gosh.sh:9097/pinss
- * @param {String} cid 
+ *        http://pin.ipfs.gosh.sh:9097/pins
+ * @param {CID | String} cid 
  * @link https://ipfs.github.io/pinning-services-api-spec/#tag/pins/paths/~1pins~1{requestid}/get
  */
 async function pinRemoteAdd(cid) {
+    /** @type {ExtendedResponse} */
     let response
+    let responseBody
     while (networkErrors < MAX_NETWORK_ERRORS) {
         verbose(`${networkErrors} POST pin ${cid}`)
-        response = await fetch(PIN_REMOTE_ENDPOINT, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ cid: cid.toString() }),
-        })
-        verbose(response.status, cid, PIN_REMOTE_ENDPOINT, await response.json())
-        verbose(ipfs_node.isOnline())
+        response = await PIN_API.post('/pins', { json: { cid: cid.toString() } })
+        responseBody = await response.json()
         if (response.ok) break
 
         if (++networkErrors > MAX_NETWORK_ERRORS) {
-            console.error(`Fetch error: ${await response.json()}`)
-            process.exit(1)
+            throw new Error(`Fetch error: ${responseBody}`)
         }
         await setTimeout(UPDATE_INTERVAL)
     }
 
-    let pinStatus
-
     while (networkErrors < MAX_NETWORK_ERRORS) {
         if (response.ok) {
-            pinStatus = await response.json()
-            if (pinStatus.status == "pinned") {
+            const { status } = responseBody
+            if (status == "failed") {
+                throw new Error(`Pinning ${cid} failed`)
+            }
+            if (status == "pinned") {
                 verbose(`CID ${cid} pinned`)
                 return true
             }
         }
 
         if (++networkErrors > MAX_NETWORK_ERRORS) {
-            console.error(`Fetch error: ${await response.json()}`)
-            process.exit(1)
+            throw new Error(`Fetch error: ${await response.json()}`)
         }
 
         // next request
         verbose(`${networkErrors} SLEEP interval ${cid}`)
         await setTimeout(UPDATE_INTERVAL)
         verbose(`${networkErrors} GET pin ${cid}`)
-        const url = `${PIN_REMOTE_ENDPOINT}/${cid}`
-        response = await fetch(url)
-        verbose(response.status, cid, url, await response.json())
+        response = await PIN_API.get(`/pins/${cid}`)
+        responseBody = await response.json()
+        verbose(response.status, cid, responseBody)
     }
 }
+
 const getIpfs = (() => {
-    /** @type {import('ipfs-core').Options} */
+    /** @type {Options} */
     const config = {
         libp2p: {
+            // @ts-ignore
             modules: {
                 connProtector: new Protector(Buffer.from(SWARM_KEY))
             }
@@ -100,7 +112,7 @@ const getIpfs = (() => {
     }
 
     /**
-     * @returns {Promise<import('ipfs-core').IPFS>}
+     * @returns {Promise<IPFS>}
      */
     return async () => {
         // !! lock strongly required because somehow create tries to create new storage(or check if exists)
@@ -115,6 +127,7 @@ const getIpfs = (() => {
 
 /**
 * @param {Buffer | String} content 
+* @returns {Promise<CID>}
 */
 async function saveToIPFS(content) {
     const ipfs = await getIpfs()
@@ -125,7 +138,8 @@ async function saveToIPFS(content) {
 }
 
 /**
-* @param {string} cid 
+* @param {CID | string} cid 
+* @returns {Promise<Buffer>}
 */
 async function loadFromIPFS(cid) {
     const ipfs = await getIpfs()
@@ -136,11 +150,12 @@ async function loadFromIPFS(cid) {
 /// testing poligon
 
 // async function main() {
+//     verbose(`start poligon`)
 //     let cids = await Promise.all([
-//         saveToIPFS('test p2p _a1'),
-//         saveToIPFS('test p2p _a2'),
-//         saveToIPFS('test p2p _a3'),
-//         saveToIPFS('test p2p _a4'),
+//         saveToIPFS('test p2p _c1'),
+//         saveToIPFS('test p2p _c2'),
+//         saveToIPFS('test p2p _c3'),
+//         saveToIPFS('test p2p _c4'),
 //     ])
 
 
@@ -157,3 +172,10 @@ module.exports = {
     loadFromIPFS,
     saveToIPFS,
 }
+
+/**
+ * @typedef {import('ipfs-core').CID} CID
+ * @typedef {import('ipfs-core').Options} Options
+ * @typedef {import('ipfs-core').IPFS} IPFS
+ * @typedef {import('ipfs-utils/src/http').ExtendedResponse} ExtendedResponse
+ */
